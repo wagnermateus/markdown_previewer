42e6d719a2d1d7b52d84363b9d9f9b228ba5a9de
wagnermateus <wagnermateus7@gmail.com>
2022-07-04 21:56:33 +0100

    add css and

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,59 @@
 import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import { CodeExample } from "./Components/CodeExample";
+import { BiCollapse, BiExpand } from "react-icons/bi";
 export function App() {
   const [markdown, setMarkdown] = useState(CodeExample);
+  const [textareaIsMaximized, setTextAreaIsMaximized] = useState(false);
+  const [previewerIsMaximized, setPreviewerIsMaximized] = useState(false);
+  function maximizeAndMinimizeTextArea() {
+    setTextAreaIsMaximized(!textareaIsMaximized);
+  }
+  function maximizeAndMinimizePreviewer() {
+    setPreviewerIsMaximized(!previewerIsMaximized);
+  }
   return (
     <div className={styles.App}>
-      <div className={styles.Editor}>
-        <div>
+      <div  className={                        
+              previewerIsMaximized ?            
+                styles.previewerMaximizedTextAreaHidden:
+                styles.Editor
+            }>
+        <div className={styles.boxHeader}>
           <strong>Editor</strong>
+          <i onClick={maximizeAndMinimizeTextArea} className={styles.btn}>
+            {!textareaIsMaximized ? <BiExpand /> : <BiCollapse />}
+          </i>
         </div>
         <div>
-          <textarea 
-          id="editor" 
-          onChange={(event) => setMarkdown(event.target.value)}
-          >{CodeExample}</textarea>
+          <textarea
+            className={
+              textareaIsMaximized? 
+               styles.maximizedTextArea
+                :styles.minimizedTextArea 
+            }
+            id="editor"
+            onChange={(event) => setMarkdown(event.target.value)}
+          >
+            {CodeExample}
+          </textarea>
         </div>
       </div>
-
-      <div className={styles.Previewer}>
-        <div>
-          <strong>Preview</strong>
+      {!textareaIsMaximized && (
+        <div className={styles.Previewer}>
+          <div className={styles.boxHeader}>
+            <strong>Preview</strong>
+            <i onClick={maximizeAndMinimizePreviewer} className={styles.btn}>
+              {!previewerIsMaximized ? <BiExpand /> : <BiCollapse />}
+            </i>
+          </div>
+          <div id="preview">
+            <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+          </div>
         </div>
-        <div id="preview">
-          <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
